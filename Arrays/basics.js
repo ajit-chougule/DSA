c451246deaf94deeaea0802f93fded5a3a1560f8
ajit-chougule <ajit.chougule@outlook.com>
2022-12-31 01:04:50 +0530

    'Delete array element' program added

--- Arrays/basics.js
+++ Arrays/basics.js
@@ -1,14 +1,24 @@
 //Insert an element in an array at given position//
+// let arr = [1,2,3,4,5,6,7,8];
+// let pos = 2;
+// let num = 100;
+
+// for(let i=arr.length-1; i>=pos; i--){
+//     arr[i+1] = arr[i];
+//     if(i == pos){
+//         arr[pos] = num; 
+//     }
+// }
+// console.log(arr);
+
+//-----------------------------------------------------
+//Delete an element from an array from given position//
 let arr = [1,2,3,4,5,6,7,8];
-let pos = 2;
-let num = 100;
+let pos = 6;
 
-for(let i=arr.length-1; i>=pos; i--){
-    arr[i+1] = arr[i];
-    if(i == pos){
-        arr[pos] = num; 
-    }
+for(let i=pos; i<arr.length-1; i++){
+    arr[i] = arr[i+1];
 }
+arr.length = arr.length-1;
 console.log(arr);
-
 
