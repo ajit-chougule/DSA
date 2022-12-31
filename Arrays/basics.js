b3fc124ec957dafffea06359e111fa37b3a9b806
ajit-chougule <ajit.chougule@outlook.com>
2022-12-31 10:44:12 +0530

    'Reverse an array' program added

--- Arrays/basics.js
+++ Arrays/basics.js
@@ -13,12 +13,24 @@
 
 //-----------------------------------------------------
 //Delete an element from an array from given position//
-let arr = [1,2,3,4,5,6,7,8];
-let pos = 6;
+// let arr = [1,2,3,4,5,6,7,8];
+// let pos = 6;
 
-for(let i=pos; i<arr.length-1; i++){
-    arr[i] = arr[i+1];
+// for(let i=pos; i<arr.length-1; i++){
+//     arr[i] = arr[i+1];
+// }
+// arr.length = arr.length-1;
+// console.log(arr);
+
+//------------------------------------------------------
+//Reverse an array//
+let arr = [1,2,3,4,5,6,7,8,9,10];
+let temp;
+let arrLen = arr.length-1;
+
+for(let i=0; i<=arrLen/2;i++){
+    temp = arr[i];
+    arr[i] = arr[arrLen-i];
+    arr[arrLen-i] = temp;
 }
-arr.length = arr.length-1;
 console.log(arr);
-
