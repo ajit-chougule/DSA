448c384f055fbecd092e7bbf6e95be6871da3a75
ajit-chougule <ajit.chougule@outlook.com>
2022-12-31 21:08:24 +0530

    'Programs for lenear search' added

--- Arrays/basics.js
+++ Arrays/basics.js
@@ -24,13 +24,47 @@
 
 //------------------------------------------------------
 //Reverse an array//
-let arr = [1,2,3,4,5,6,7,8,9,10];
-let temp;
-let arrLen = arr.length-1;
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// let temp;
+// let arrLen = arr.length-1;
 
-for(let i=0; i<=arrLen/2;i++){
-    temp = arr[i];
-    arr[i] = arr[arrLen-i];
-    arr[arrLen-i] = temp;
+// for(let i=0; i<=arrLen/2;i++){
+//     temp = arr[i];
+//     arr[i] = arr[arrLen-i];
+//     arr[arrLen-i] = temp;
+// }
+// console.log(arr);
+
+//-------------------------------------------------------
+//Linear Search//First Occurance//
+// function lenearSearch(arr,e){
+//     for(let i=0; i<arr.length; i++){
+//         if(arr[i] == e){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+// let e = 70;
+// console.log(lenearSearch(arr,e));
+
+//-------------------------------------------------------
+//Linear Search//Multiple Occurances//
+function lenearSearch(arr,e){
+    let positions = [];
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] == e){
+            positions.push(i);
+        }
+    }
+    if(positions.length == 0){
+        return -1;
+    }
+    return positions;
 }
-console.log(arr);
+
+let arr = [1,2,3,4,5,3,6,7,8,3,9];
+let e = 3;
+console.log(lenearSearch(arr,e));
